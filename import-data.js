a2d2a049c097941798883f9cc0779ec066623622
jpeill <peilleron.jeremy@gmail.com>
2022-09-30 16:28:58 +0200

    TD2 finit

--- import-data.js
+++ import-data.js
@@ -1,18 +1,18 @@
-import mongoose from 'mongoose';
+const mongoose=require('mongoose');
+require('dotenv').config('./.env')
+const filmingLocations = require('C:/Users/peill/Documents/Personnel/COURS/A4/web/TD1/secure-web-dev-workshop1/lieux-de-tournage-a-paris.json')
+
 const { Schema } = mongoose;
 
-const blogSchema = new Schema({
+const LocationsSchema = new Schema({
     filmType: String,
     filmProducerName: String,
     endDate: Date,
     filmName: String,
     district: Number,
     geolocation: {
-        coordinates: [
-            Number,
-            Number
-        ],
-        type: String
+        coordinates: [Number],
+        type: String,
     },
     sourceLocationId: String,
     filmDirectorName: String,
@@ -20,3 +20,35 @@ const blogSchema = new Schema({
     startDate: Date,
     year: Number,
 });
+
+
+mongoose.connect(process.env.MONGO_URI).then((success)=>console.log("connecté"))
+
+const Locations =  mongoose.model('Locations',LocationsSchema);
+
+
+function CreateLocations(filmingLocations){
+    let promiseschunk = []
+    for(const film in filmingLocations){
+        f=filmingLocations[film].fields
+        const Location = new Locations({
+            filmType: f.type_tournage,
+            filmProducerName: f.nom_producteur,
+            endDate: f.date_fin,
+            filmName: f.nom_tournage,
+            district: f.ardt_lieu,
+            geolocation: {
+                coordinates: f.geo_shape.coordinates,
+                type: f.geo_shape.type,
+            },
+            sourceLocationId: f.id_lieu,
+            filmDirectorName: f.nom_realisateur,
+            address: f.adresse_lieu,
+            startDate: f.date_debut,
+            year: f.annee_tournage,
+        });
+    }
+    
+}
+
+
